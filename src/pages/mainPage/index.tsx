9663f81529538db974118de48e536944e086ae3c
ramon <ramonlindo2311@gmail.com>
2024-04-02 16:32:27 -0300

    feat: implement header

--- src/pages/mainPage/index.tsx
+++ src/pages/mainPage/index.tsx
@@ -1,7 +1,59 @@
-import React from 'react';
-
+import './index.css';
 const MainPage = () => {
-  return <div>MainPage</div>;
+  let clicktest = (e: any) => {
+    console.log(e.target.alt);
+  };
+  return (
+    <>
+      <div className="containerAllMainbackGround">
+        <div className="containerAllMain">
+          <header className="HeaderContainer">
+            <div className="containerTitleAndImg">
+              <div className="containerImgHeader">
+                <img
+                  src="https://jacaimages.vercel.app/imgs/logos/logogelado.svg"
+                  alt="logo geladon"
+                  className="geladonImg"
+                />
+              </div>
+              <span className="spanTitle">El Geladon</span>
+            </div>
+            <div className="containerOptions">
+              <div className="containerImg" onClick={clicktest}>
+                <img
+                  src="https://jacaimages.vercel.app/imgs/logos/paleta.svg"
+                  alt="img to crate a new paleta "
+                  className="optionImg"
+                />
+              </div>
+              <div className="containerImg" onClick={clicktest}>
+                <img
+                  src="https://jacaimages.vercel.app/imgs/logos/atualizar.svg"
+                  alt="image to edit a paleta"
+                  className="optionImg"
+                />
+              </div>
+              <div className="containerImg" onClick={clicktest}>
+                <img
+                  src="https://jacaimages.vercel.app/imgs/logos/deletar.svg"
+                  alt="image to delete a paleta"
+                  className="optionImg"
+                />
+              </div>
+              <div className="containerImg" onClick={clicktest}>
+                <img
+                  src="https://jacaimages.vercel.app/imgs/logos/sacola.svg"
+                  alt="img to see the grocery bag "
+                  className="optionImg"
+                />
+              </div>
+            </div>
+          </header>
+          <div className="containerAllPaletas">paletas</div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default MainPage;
